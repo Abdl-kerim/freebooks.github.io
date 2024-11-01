d0ae6c02c502a8707d246a0b0ee9b0eef85158a4
voidweaver <142108264+Abdl-kerim@users.noreply.github.com>
2024-11-01 20:38:11 +0100

    a.b

--- index.php
+++ index.php
@@ -1,12 +1,10 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="author" content="Abdel Kerim">
     <meta name="robots" content="noindex, nofollow">
-    
     <link rel="stylesheet" href="./src/style/style.css" crossorigin="anonymous">
     <link rel="apple-touch-icon" href="./src/assets/head/apple-touch-icon.png" crossorigin="anonymous">
     <link rel="shortcut icon" href="./src/assets/head/favicon.ico" crossorigin="anonymous">
@@ -14,29 +12,19 @@
     <link rel="icon" type="image/png" sizes="32x32" href="./src/assets/head/favicon-32x32.png" crossorigin="anonymous">
     <link rel="icon" type="image/png" sizes="192x192" href="./src/assets/head/android-chrome-192x192.png" crossorigin="anonymous">
     
-    <title>Libros Gratis</title>
+    <title>Free Books</title>
 </head>
 <body id="body">
-
-    <!-- Encabezado principal con selector de tema -->
-    <header id="header">
-        <h1>Libros Gratis</h1>
-    </header>
-
-    <!-- Contenido principal -->
     <main id="main">
         <section id="cont" aria-labelledby="h1_section">
             <h1 id="h1_section">Selección de libros</h1>
-            <!-- Los libros se cargarán aquí dinámicamente desde JavaScript -->
         </section>
     </main>
 
-    <!-- Pie de página -->
     <footer id="footer">
-        <small id="copyr">Creado por: Abdel Kerim, 2023&copy;</small>
+        <small id="copyr"><?php echo date("Y"); ?></small>
     </footer>
 
-    <!-- Archivo JavaScript principal -->
     <script src="./src/js/main.js"></script>
 </body>
 </html>
